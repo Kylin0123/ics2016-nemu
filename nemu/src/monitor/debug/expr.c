1d4c898003b038dcde7d3c82ff4e178ce63b9ded
tracer-ics2016 <tracer@njuics.org>
2016-09-04 03:11:50 +0000

    >  compile NEMU 151220015 jack Linux 09c15dbe4be0 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  03:11:50 up  1:28,  1 user,  load average: 0.00, 0.00, 0.00 73e205028fc6430295ca7dc7cd3e0c72c91b345c

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -204,7 +204,8 @@ uint32_t expr(char *e, bool *success) {
 	}
 	/* TODO: Insert codes to evaluate the expression. */
 	//panic("please implement me");
-	*success = true;
+	printf("!!!!!!!!!!!!!!!!\n");
+	//*success = true;
 	printf("%d", nr_token);
 	return eval(0, nr_token - 1);
 	//return 0;
