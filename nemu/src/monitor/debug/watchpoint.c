7ffe93644dcf7e05a2daeb6c6bb239bae5b1d2eb
tracer-ics2016 <tracer@njuics.org>
2016-09-06 08:11:32 +0000

    >  compile NEMU 151220015 jack Linux 09c15dbe4be0 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  08:11:32 up  1:35,  1 user,  load average: 0.00, 0.00, 0.00 9017a443eb99b8585e1bfbcb1efd9b683c1578e

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -20,4 +20,47 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
+WP* new_wp(){
+	if(head == NULL){
+		WP *temp = free_;
+		free_ = free_->next;
+		head = temp;
+		head->next = NULL;
+		return temp;
+	}	
+	else if(head != NULL && free_ != NULL){
+		WP *temp = free_;
+		free_ = free_->next;
+		temp->next = head;
+		head = temp;
+		return temp;
+	}
+	else if(free_ == NULL){
+		assert(0);
+	}
+	else
+		return NULL;
+}
 
+void free_wp(WP* wp){
+	if(wp == head){
+		head = head->next;
+		wp->next = free_;
+		free_ = wp;
+	}
+	else if(wp->next == NULL){
+		WP* temp = head;
+		for(;temp->next->next != NULL; temp = temp->next);
+		temp->next = NULL;
+		wp->next = free_;
+		free_ = wp;
+	}
+	else{
+		WP* temp = head;
+		for(;temp->next->next != wp; temp = temp->next);
+		WP *temp2 = temp->next;
+		temp->next = wp;
+		temp2->next = free_;
+		free_ = temp2;
+	}
+}
