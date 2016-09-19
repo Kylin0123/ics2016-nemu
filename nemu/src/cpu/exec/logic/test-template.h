f1290fe63c29e7a5e752b416d9755ecdf3be5a4b
tracer-ics2016 <tracer@njuics.org>
2016-09-19 06:41:51 -0700

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  06:41:51 up 1 day,  4:43,  7 users,  load average: 0.15, 0.06, 0.06 9aca1350bf1ca1226f6b798a258a235ec3a8f3dc

---
+++ nemu/src/cpu/exec/logic/test-template.h
@@ -0,0 +1,38 @@
+/*************************************************************************
+	> File Name: test-template.h
+	> Author: 
+	> Mail: 
+	> Created Time: Mon 19 Sep 2016 05:58:25 AM PDT
+ ************************************************************************/
+
+#include "cpu/exec/temlate-start.h"
+
+#define instr test
+
+static void do_execute(){
+    DATA_TYPE temp = op_dest->val & op_src->val;
+    cpu.eflags._sf = MSB(temp);
+    
+    if(temp == 0)
+        cpu.eflags._zf = 1;
+    else
+        cpu.eflags._zf = 0;
+    int temp_pf = temp & 1;
+    int temp_test = 2;
+    int i;
+    for(i = 1; i < 8; i++){
+        int temp_k = (temp & temp_test) >> i;
+        temp_pf = !(temp_k ^ temp_fp);
+        temp_test << 1;
+    }
+    cpu.eflags._pf = temp_pf;
+    cpu.eflags._cf = 0;
+    cpu.eflags._of = 0;
+}
+
+make_instr_helper(i2r)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+
+#include "cpu/exec/template-end.h"
+
