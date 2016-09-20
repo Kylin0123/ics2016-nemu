324dbf0c7d7f824661c8c1bce33675eba7138038
tracer-ics2016 <tracer@njuics.org>
2016-09-20 05:28:23 -0700

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  05:28:23 up 1 day,  9:39,  7 users,  load average: 0.00, 0.01, 0.05 f630e21338e8646690047925263fbbc073d836b7

---
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -0,0 +1,33 @@
+/*************************************************************************
+	> File Name: cmp-template.h
+	> Author: 
+	> Mail: 
+	> Created Time: Tue 20 Sep 2016 02:35:15 AM PDT
+ ************************************************************************/
+
+#include "cpu/exec/template-start.h"
+
+#define instr cmp
+
+static void do_execute(){
+    DATA_TYPE temp = op_dest->val - op_src->val;
+    if(temp == 0)
+        cpu.eflags._zf = 1;
+    else
+        cpu.eflags._zf = 0;
+    
+    if(temp < 0)
+        cpu.eflags._cf = 1;
+    else if(temp > 0){
+        cpu.eflags._cf = 0;
+    }
+        
+
+}
+
+make_instr_helper(i2r)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+#include "cpu/exec/template-end.h"
---
+++ nemu/src/cpu/exec/arith/cmp.h
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: cmp.h
+	> Author: 
+	> Mail: 
+	> Created Time: Tue 20 Sep 2016 04:27:08 AM PDT
+ ************************************************************************/
+
+#ifndef _CMP_H
+#define _CMP_H
+
+make_helper(cmp_i2rm_l);
+
+make_helper(cmp_i2rm_v);
+
+#endif
