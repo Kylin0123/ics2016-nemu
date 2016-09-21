5d28b5870d7c0e93d01334a638a4e04c912424a0
tracer-ics2016 <tracer@njuics.org>
2016-09-21 01:04:29 -0700

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  01:04:29 up 1 day, 14:34, 10 users,  load average: 0.31, 0.26, 0.19 ce73bfc8e1e7fb13c3c7b546bbb6681bdf21b81

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -9,7 +9,7 @@
 #define instr call
 
 static void do_execute(){
-    uint32_t data = cpu.eip + DATA_BYTE;
+    uint32_t data = cpu.eip + 4;
     cpu.esp -= 4;
     MEM_W(cpu.esp, data);
     cpu.eip = data + op_src->val;
