719bf558d1574ba84ae1a077e4d2f18ef90b2ccf
tracer-ics2016 <tracer@njuics.org>
2016-09-23 23:07:48 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  23:07:48 up 1 day, 18:44, 13 users,  load average: 0.07, 0.03, 0.05 725d2702f0f70f0f8fbda05cba553fd574854f12

--- nemu/src/cpu/exec/control/jbe-template.h
+++ nemu/src/cpu/exec/control/jbe-template.h
@@ -10,7 +10,7 @@
 #define instr jbe
 
 static void do_execute(){
-    uint32_t rel = op_src->val;
+    int rel = op_src->val;
     printf("aaaa:%d\n", rel);
     if(cpu.eflags._zf == 1 || cpu.eflags._cf == 1)
         cpu.eip = cpu.eip + rel;
