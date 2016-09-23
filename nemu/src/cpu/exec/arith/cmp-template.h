5a387f1cc726e4f7519593bc4303b237a4f3a891
tracer-ics2016 <tracer@njuics.org>
2016-09-23 22:53:18 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  22:53:18 up 1 day, 18:30, 13 users,  load average: 0.38, 0.17, 0.12 3acdd2bf4d1bee383fa705751b39ba7d660f7e3

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -10,7 +10,7 @@
 #define instr cmp
 
 static void do_execute(){
-    DATA_TYPE temp = op_dest->val - op_src->val;
+    //DATA_TYPE temp = op_dest->val - op_src->val;
     if(op_dest->val == op_src->val)
         cpu.eflags._zf = 1;
     else
@@ -21,12 +21,12 @@ static void do_execute(){
     else if(op_dest->val > op_src->val){
         cpu.eflags._cf = 0;
     }
-     printf("dest:%d\n", op_dest->val);
+     /*printf("dest:%d\n", op_dest->val);
     printf("src:%d\n", op_src->val);
      printf("zf:%d\n", cpu.eflags._zf); 
      printf("cf:%d\n", cpu.eflags._cf); 
      printf("temp:%d\n", temp); 
-    
+    */
 }
 
 make_instr_helper(i2r)
--- nemu/src/cpu/exec/control/jbe-template.h
+++ nemu/src/cpu/exec/control/jbe-template.h
@@ -10,8 +10,10 @@
 #define instr jbe
 
 static void do_execute(){
+    uint32_t rel = op_src->val;
+    printf("aaaa:%d\n", rel);
     if(cpu.eflags._zf == 1 || cpu.eflags._cf == 1)
-        cpu.eip = cpu.eip + op_src->val;
+        cpu.eip = cpu.eip + rel;
     print_asm_template1();
 }
 
