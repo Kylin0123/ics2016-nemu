bd4d66b37a25603768f755b7e13bc103635f6549
tracer-ics2016 <tracer@njuics.org>
2016-09-24 22:37:06 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  22:37:06 up 2 days,  4:43, 13 users,  load average: 0.01, 0.04, 0.07 afb51ca831728be38489f65819aff27f8fd0e225

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -34,12 +34,12 @@ make_instr_helper(rm2r)
 make_helper(concat(sub_i_b2rm_, SUFFIX)){
     int len = concat(decode_rm_, SUFFIX)(eip + 1);
     printf("len:%d\n", len);
-    len = decode_i_b(eip + 1 + len);
-    printf("len:%d\n", len);
+    int len2 = decode_i_b(eip + 1 + len);
+    printf("len2:%d\n", len2);
     //snprintf(op_dest->str, OP_STR_SIZE, "$0x%x", op_src->imm);
     do_execute();
     printf("eip:%x\n", cpu.eip);
-    return len;
+    return len + len2;
 }
 #endif
 
