17cc25d72f0575a8086a8fd50d2678665231a92e
tracer-ics2016 <tracer@njuics.org>
2016-09-24 12:07:24 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  12:07:24 up 1 day, 20:26, 13 users,  load average: 0.00, 0.03, 0.10 44f37739320326f1c1c9c7feea2bddda7fa05728

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -21,12 +21,12 @@ static void do_execute(){
     else if(op_dest->val > op_src->val){
         cpu.eflags._cf = 0;
     }
-     /*printf("dest:%d\n", op_dest->val);
-    printf("src:%d\n", op_src->val);
+     printf("dest:%d\n", op_dest->val);
+     printf("src:%d\n", op_src->val);
      printf("zf:%d\n", cpu.eflags._zf); 
      printf("cf:%d\n", cpu.eflags._cf); 
-     printf("temp:%d\n", temp); 
-    */
+     //printf("temp:%d\n", temp); 
+    
 }
 
 make_instr_helper(i2r)
