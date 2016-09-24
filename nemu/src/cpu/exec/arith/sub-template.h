001f0793e62968b2c2c10884f6d8f7e9ace7fe5f
tracer-ics2016 <tracer@njuics.org>
2016-09-24 21:00:16 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  21:00:16 up 2 days,  3:06, 13 users,  load average: 0.08, 0.16, 0.13 a831288c9fc22a517260e304de087f95fa1807f9

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -34,22 +34,7 @@ make_instr_helper(rm2r)
 make_helper(concat(sub_i_b2rm_, SUFFIX)){
     Operand rm, reg;
     int len = read_ModR_M(eip, &rm, &reg);
-    /*m.val = instr_fetch(eip, 1);
-    reg.type = OP_TYPE_REG;
-    reg.reg = m.reg;
-    if(m.mod == 3){
-        rm.type = OP_TYPE_REG;
-        rm.reg = m.R_M;
-        switch(rm.size){
-            case 1: rm.val = reg_b(m.R_M);break;
-            case 2: rm.val = reg_w(m.R_M);break;
-            case 4: rm.val = reg_l(m.R_M);break;
-            default: assert(0);
-        }
-    }
-    else{
-        int instr_len = load_addr()
-    }*/
+    printf("len:%d\n", len);
     //snprintf(op_dest->str, OP_STR_SIZE, "$0x%x", op_src->imm);
     do_execute();
     return len;
