25fc449bd23d0bf5af066482ce02078ea016e872
tracer-ics2016 <tracer@njuics.org>
2016-09-27 21:28:24 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  21:28:24 up 2 days, 17:51, 13 users,  load average: 0.14, 0.09, 0.07 ddec2959fe0ff36dbd68d361ab2a68feecb4ffdb

--- nemu/src/cpu/exec/logic/or-template.h
+++ nemu/src/cpu/exec/logic/or-template.h
@@ -7,7 +7,9 @@ static void do_execute () {
 	OPERAND_W(op_dest, result);
 
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	//panic("please implement me");
+    cpu.eflags._cf = 0;
+    cpu.eflags._of = 0;
 
 	print_asm_template2();
 }
