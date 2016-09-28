831376c5aef47a1b5c5d4a9c591ad3fdb8c97771
tracer-ics2016 <tracer@njuics.org>
2016-09-28 19:06:06 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  19:06:06 up 3 days, 1 min, 13 users,  load average: 0.14, 0.12, 0.08 6208d841ca702298dae31243bc9b2178be654c28

--- nemu/src/cpu/exec/arith/add-template.h
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -13,6 +13,40 @@ static void do_execute(){
     DATA_TYPE result = op_dest->val + op_src->val;
     OPERAND_W(op_dest, result);
     //todo : eflags is unfinished.
+    cpu.eflags._zf = !result;
+    cpu.eflags._sf = MSB(result);
+    if(MSB(op_dest->val) == 0 && MSB(op_src->val) == 0 && MSB(result) == 0){
+        cpu.eflags._cf = 0;
+        cpu.eflags._of = 0;
+    }
+    else if(MSB(op_dest->val) == 0 && MSB(op_src->val) == 0 && MSB(result) == 1){
+        cpu.eflags._cf = 0;
+        cpu.eflags._of = 1;
+    }
+    else if(MSB(op_dest->val) == 0 && MSB(op_src->val) == 1 && MSB(result) == 0){
+        cpu.eflags._cf = 1;
+        cpu.eflags._of = 0;
+    }
+    else if(MSB(op_dest->val) == 0 && MSB(op_src->val) == 1 && MSB(result) == 1){
+        cpu.eflags._cf = 0;
+        cpu.eflags._of = 0;
+    }
+    else if(MSB(op_dest->val) == 1 && MSB(op_src->val) == 0 && MSB(result) == 0){
+        cpu.eflags._cf = 1;
+        cpu.eflags._of = 0;
+    }
+    else if(MSB(op_dest->val) == 1 && MSB(op_src->val) == 0 && MSB(result) == 1){
+        cpu.eflags._cf = 0;
+        cpu.eflags._of = 0;
+    }
+    else if(MSB(op_dest->val) == 1 && MSB(op_src->val) == 1 && MSB(result) == 0){
+        cpu.eflags._cf = 1;
+        cpu.eflags._of = 1;
+    }
+    else if(MSB(op_dest->val) == 1 && MSB(op_src->val) == 1 && MSB(result) == 1){
+        cpu.eflags._cf = 1;
+        cpu.eflags._of = 0;
+    }
     /*printf("op_dest:%d\n", op_dest->val);
     printf("op_src:%d\n", op_src->val);
     printf("result:%d\n", result);
@@ -26,7 +60,8 @@ make_instr_helper(r2rm)
 make_instr_helper(rm2r)
 
 #if DATA_BYTE == 2 || DATA_BYTE == 4
-
+make_instr_helper(si2rm)
+/*
 make_helper(concat(add_i_b2rm_, SUFFIX)){
     int len = concat(decode_rm_, SUFFIX)(eip + 1);
     op_dest->type = op_src->type;
@@ -48,7 +83,7 @@ make_helper(concat(add_i_b2rm_, SUFFIX)){
     do_execute();
     return 1 + len + len2;
 }
-
+*/
 #endif
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/arith/add.h
+++ nemu/src/cpu/exec/arith/add.h
@@ -15,9 +15,10 @@ make_helper(add_rm2r_b);
 
 make_helper(add_i2r_v);
 make_helper(add_i2rm_v);
+make_helper(add_si2rm_v);
 make_helper(add_r2rm_v);
 make_helper(add_rm2r_v);
 
-make_helper(add_i_b2rm_v);
+//make_helper(add_i_b2rm_v);
 
 #endif
