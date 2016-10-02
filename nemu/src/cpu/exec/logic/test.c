c91ee235557fa0b85cc916fe3b5c0e578bca7fc8
tracer-ics2016 <tracer@njuics.org>
2016-10-02 19:36:56 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  19:36:56 up 3 days,  3:34, 12 users,  load average: 0.14, 0.12, 0.13 a0acf406348497b1d40a05e3d05668d96bbe4092

--- nemu/src/cpu/exec/logic/test.c
+++ nemu/src/cpu/exec/logic/test.c
@@ -20,5 +20,6 @@
 #undef DATA_BYTE
 
 make_helper_v(test_i2r)
+make_helper_v(test_i2a)
 make_helper_v(test_i2rm)
 make_helper_v(test_r2rm)
--- nemu/src/cpu/exec/logic/test.h
+++ nemu/src/cpu/exec/logic/test.h
@@ -9,10 +9,12 @@
 #define _TEST_H
 
 make_helper(test_i2r_b);
+make_helper(test_i2a_b);
 make_helper(test_i2rm_b);
 make_helper(test_r2rm_b);
 
 make_helper(test_i2r_v);
+make_helper(test_i2a_v);
 make_helper(test_i2rm_v);
 make_helper(test_r2rm_v);
 
