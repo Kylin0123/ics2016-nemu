b0fdae008e9f2319da1697d77028862e4d52e329
tracer-ics2016 <tracer@njuics.org>
2016-10-07 21:57:04 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  21:57:04 up 3 days, 12:08, 13 users,  load average: 0.14, 0.16, 0.18 68fa65b192cd89d1356a1dc3b1648ca7bcba1899

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -58,5 +58,125 @@ static void do_execute(){
 make_instr_helper(si)
 #undef instr
 
+#define instr jl
+static void do_execute(){
+    int32_t rel = op_src->val;
+    if(cpu.eflags._sf != cpu.eflags._of)
+        cpu.eip += rel;
+    print_asm_template1();
+}
+make_instr_helper(si)
+#undef instr
+
+#define instr ja
+static void do_execute(){
+    int32_t rel = op_src->val;
+    if(cpu.eflags._cf == 0 && cpu.eflags._zf == 0)
+        cpu.eip += rel;
+    print_asm_template1();
+}
+make_instr_helper(si)
+#undef instr
+
+#define instr jae
+static void do_execute(){
+    int32_t rel = op_src->val;
+    if(cpu.eflags._cf == 0)
+        cpu.eip += rel;
+    print_asm_template1();
+}
+make_instr_helper(si)
+#undef instr
+
+#define instr jb
+static void do_execute(){
+    int32_t rel = op_src->val;
+    if(cpu.eflags._cf == 1)
+        cpu.eip += rel;
+    print_asm_template1();
+}
+make_instr_helper(si)
+#undef instr
+
+#define instr jcxz
+static void do_execute(){
+    int32_t rel = op_src->val;
+    if(cpu.gpr[1]._16 == 0)
+        cpu.eip += rel;
+    print_asm_template1();
+}
+make_instr_helper(si)
+#undef instr
+
+#define instr jge
+static void do_execute(){
+    int32_t rel = op_src->val;
+    if(cpu.eflags._sf == cpu.eflags._of)
+        cpu.eip += rel;
+    print_asm_template1();
+}
+make_instr_helper(si)
+#undef instr
+
+#define instr jno
+static void do_execute(){
+    int32_t rel = op_src->val;
+    if(cpu.eflags._of == 0)
+        cpu.eip += rel;
+    print_asm_template1();
+}
+make_instr_helper(si)
+#undef instr
+
+#define instr jnp
+static void do_execute(){
+    int32_t rel = op_src->val;
+    if(cpu.eflags._pf == 0)
+        cpu.eip += rel;
+    print_asm_template1();
+}
+make_instr_helper(si)
+#undef instr
+
+#define instr jns
+static void do_execute(){
+    int32_t rel = op_src->val;
+    if(cpu.eflags._sf == 0)
+        cpu.eip += rel;
+    print_asm_template1();
+}
+make_instr_helper(si)
+#undef instr
+
+#define instr jo
+static void do_execute(){
+    int32_t rel = op_src->val;
+    if(cpu.eflags._of == 1)
+        cpu.eip += rel;
+    print_asm_template1();
+}
+make_instr_helper(si)
+#undef instr
+
+#define instr jp
+static void do_execute(){
+    int32_t rel = op_src->val;
+    if(cpu.eflags._pf == 1)
+        cpu.eip += rel;
+    print_asm_template1();
+}
+make_instr_helper(si)
+#undef instr
+
+#define instr js
+static void do_execute(){
+    int32_t rel = op_src->val;
+    if(cpu.eflags._sf == 1)
+        cpu.eip += rel;
+    print_asm_template1();
+}
+make_instr_helper(si)
+#undef instr
+
 #include "cpu/exec/template-end.h"
 
