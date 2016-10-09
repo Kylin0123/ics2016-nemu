050f5f3c82fe2047ae103020249b59d0b3575deb
tracer-ics2016 <tracer@njuics.org>
2016-10-09 14:30:31 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  14:30:31 up 3 days, 15:56, 12 users,  load average: 0.16, 0.14, 0.14 1774cb73b8cc7cb28c7d0cd5e70ca27f815f95d

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -65,12 +65,12 @@ make_group(group2_cl_v,
 /* 0xf6 */
 make_group(group3_b,
 	test_i2rm_b, inv, not_rm_b, inv, 
-	inv, inv, inv, idiv_rm_b)
+	inv, imul_rm2a_b, inv, idiv_rm_b)
 
 /* 0xf7 */
 make_group(group3_v,
 	test_i2rm_v, inv, not_rm_v, inv, 
-	inv, inv, inv, idiv_rm_v)
+	inv, imul_rm2a_v, inv, idiv_rm_v)
 
 /* 0xfe */
 make_group(group4,
@@ -204,7 +204,7 @@ helper_fun _2byte_opcode_table [256] = {
 /* 0xa0 */	inv, inv, inv, inv, 
 /* 0xa4 */	inv, inv, inv, inv,
 /* 0xa8 */	inv, inv, inv, inv,
-/* 0xac */	inv, inv, inv, inv,
+/* 0xac */	inv, inv, inv, imul_rm2r_v,
 /* 0xb0 */	inv, inv, inv, inv, 
 /* 0xb4 */	inv, inv, movz_rmb2r_v, movz_rmw2r_l, 
 /* 0xb8 */	inv, inv, inv, inv,
