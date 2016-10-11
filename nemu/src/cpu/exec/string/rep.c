a45424291918968ccd1d695ecaa020310e2156f6
tracer-ics2016 <tracer@njuics.org>
2016-10-11 09:44:32 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  09:44:32 up 4 days,  1:32, 13 users,  load average: 0.04, 0.24, 0.64 b7f06ca0f66139e0d4212067b542e74b53e91802

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -26,7 +26,7 @@ make_helper(rep) {
 				);
 
 			/* TODO: Jump out of the while loop if necessary. */
-
+            if(cpu.ecx == 0) break;
 		}
 		len = 1;
 	}
@@ -53,7 +53,7 @@ make_helper(repnz) {
 			  );
 
 		/* TODO: Jump out of the while loop if necessary. */
-
+        if(cpu.ecx == 0) break;
 	}
 
 #ifdef DEBUG
