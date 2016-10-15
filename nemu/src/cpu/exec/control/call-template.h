7e80f3c4bf01e07387ecb5b300e252b425b946ad
tracer-ics2016 <tracer@njuics.org>
2016-10-15 15:18:35 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  15:18:35 up 5 days,  6:07, 17 users,  load average: 0.31, 0.21, 0.18 9fa83f99b8676e7b412100e24659626852fbb61f

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -21,7 +21,7 @@ static void do_execute(){
 
 #if DATA_BYTE == 2
 make_helper(call_rm_w){
-    decode_rm_w(eip);
+    decode_rm_l(eip);
     cpu.esp -= 4;
     MEM_W(cpu.esp, cpu.eip + 2);
     printf("eip1:%x\n", op_src->val);
