7b26ea58b4e7e1f2d76b325097bba1c940d594df
tracer-ics2016 <tracer@njuics.org>
2016-10-15 17:41:36 +0800

    >  run 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  17:41:36 up 5 days,  8:29, 17 users,  load average: 0.26, 0.11, 0.07 1a7e898e3c7cd3ee4f00c5416b61f8a199dd7d

--- testcase/src/quadratic-eq.c
+++ testcase/src/quadratic-eq.c
@@ -4,11 +4,11 @@
 /* solve 0.5x^2 + 0.5x - 0.5 = 0 */
 
 int main() {
-	//FLOAT a = f2F(0.5);
-	//FLOAT b = f2F(0.5);
+	FLOAT a = f2F(0.5);
+	FLOAT b = f2F(0.5);
 	FLOAT c = f2F(-0.5);
 //nemu_assert(1);
-	/*FLOAT dt = F_mul_F(b, b) - F_mul_F(F_mul_int(a, 4), c);
+	FLOAT dt = F_mul_F(b, b) - F_mul_F(F_mul_int(a, 4), c);
 	FLOAT sqrt_dt = sqrt(dt);
 //nemu_assert(0);
 	FLOAT x1 = F_div_F(-b + sqrt_dt, F_mul_int(a, 2));
@@ -18,7 +18,6 @@ int main() {
 	FLOAT x2_ans = f2F(-1.618);
 
 	nemu_assert(Fabs(x1_ans - x1) < f2F(1e-4));
-	nemu_assert(Fabs(x2_ans - x2) < f2F(1e-4));*/
-	c++;
+	nemu_assert(Fabs(x2_ans - x2) < f2F(1e-4));
 	return 0;
 }
