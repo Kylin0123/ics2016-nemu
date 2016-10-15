5bc96273ec138282dbad5f17fe1c2344d1ff4ace
tracer-ics2016 <tracer@njuics.org>
2016-10-15 17:25:59 +0800

    >  run 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  17:25:59 up 5 days,  8:13, 17 users,  load average: 0.00, 0.01, 0.05 a74cb6664d71ac5ae9ad05dbab8d57292be90037

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -54,7 +54,7 @@ FLOAT f2F(float a) {
 	uint32_t tail = IntA & 0x7fffff;
     tail = tail | 0x800000;
     uint32_t exp = (IntA & 0x7f800000) >> 23;
-    nemu_assert(exp != 0x01111110);
+    nemu_assert(exp == 0x01111110);
     uint32_t flag = IntA & 0x80000000;;
     FLOAT result = 0;
     if(exp == 0){
