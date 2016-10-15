1f312e1c9248623bd494276eee9678c1fe85a71b
tracer-ics2016 <tracer@njuics.org>
2016-10-15 16:45:46 +0800

    >  run 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  16:45:46 up 5 days,  7:33, 17 users,  load average: 0.33, 0.13, 0.13 a6e6ccf54475be8f8622eeb72ff0b333f45c1fb0

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -92,7 +92,7 @@ FLOAT sqrt(FLOAT x) {
 	do {
         nemu_assert(1);
 		dt = F_div_int((F_div_F(x, t) - t), 2);
-		nemu_assert(0);
+		nemu_assert(1);
         t += dt;
 	} while(Fabs(dt) > f2F(1e-4));
 
