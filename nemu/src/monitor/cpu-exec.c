89d547d61fa7155ce03d226eacc27c078155d7d4
tracer-ics2016 <tracer@njuics.org>
2016-10-18 20:16:58 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  20:16:58 up 5 days, 15:45, 19 users,  load average: 0.16, 0.11, 0.14 41c6ef36a2570b9a2d96898ff7c09a129ec05f24

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -81,7 +81,8 @@ void cpu_exec(volatile uint32_t n) {
 			bool *success = false;
 			uint32_t exprValue = expr(tempWP->expr, success);
 			uint32_t readValue = swaddr_read( exprValue, 4);
-            printf("wp_test:exprValue:%x\nreadValue:%x\n", exprValue, readValue);
+            printf("tempexpr:%s\n", tempWP->expr);
+            printf("exprValue:%x\nreadValue:%x\n", exprValue, readValue);
             printf("tempValue:%x\n", tempWP->value);
 			if(tempWP->value != exprValue){
 				printf("Watchpoint %d changed:%d->%d\n", tempWP->NO, tempWP->value, readValue);
