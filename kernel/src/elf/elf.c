da7395dc46faa4581d1c63733e48160885790ee2
tracer-ics2016 <tracer@njuics.org>
2016-10-20 13:07:09 +0800

    >  compile kernel 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  13:07:09 up 6 days,  3:46, 19 users,  load average: 0.06, 0.04, 0.05 2c93d91ed535795bff0fb2cd6e7dcf6a7f6aec5e

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -29,6 +29,7 @@ uint32_t loader() {
 #endif
 
 	elf = (void*)buf;
+    nemu_assert(elf->e_entry == 0x8000ca);
 
 	/* TODO: fix the magic number with the correct one */
 	/*const uint32_t elf_magic = 0x7f454c46;
@@ -54,7 +55,6 @@ uint32_t loader() {
 			 */
              memset((void *)ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
              elf->e_entry = ph->p_vaddr;
-             nemu_assert(elf->e_entry == 0x8000ca);
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
 			extern uint32_t cur_brk, max_brk;
