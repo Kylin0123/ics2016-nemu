72e2a20e8ea001d3ad04a38e7077487e3bed7576
tracer-ics2016 <tracer@njuics.org>
2016-10-20 15:17:55 +0800

    >  compile kernel 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  15:17:55 up 6 days,  4:41, 19 users,  load average: 0.00, 0.04, 0.05 8c10674cfb4b0968a6c139971c2ac2b36fa6af5

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -51,7 +51,7 @@ uint32_t loader() {
              nemu_assert(ph->p_vaddr == 0x800000 || ph->p_vaddr == 0x8011c0);
              //uint8_t mybuf[4096];
 			 //ramdisk_read(mybuf, *buf + ph->p_offset, ph->p_filesz);
-             memcpy( (void *)ph->p_vaddr, buf + ph->p_offset, ph->p_filesz);
+             memcpy( (void *)ph->p_vaddr, &buf[ph->p_offset], ph->p_filesz);
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
