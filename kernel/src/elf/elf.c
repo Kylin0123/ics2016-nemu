b07d4ef235a427120872917d4012152b9119d625
tracer-ics2016 <tracer@njuics.org>
2016-10-20 21:59:12 +0800

    >  compile kernel 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  21:59:12 up 6 days,  8:12, 19 users,  load average: 0.03, 0.04, 0.05 751b35ec833e353d8650ac43aa97bced80d0c3

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -48,10 +48,10 @@ uint32_t loader() {
              //nemu_assert(0);
              uint8_t mybuf[0x2000000];
              ramdisk_read(mybuf, 0, 0x2000000);
-             nemu_assert(*(mybuf + 4096) == 0xbd);
-             nemu_assert(ph->p_offset == 0x000000 || ph->p_offset == 0x1c0);
-             nemu_assert(ph->p_vaddr == 0x800000 || ph->p_vaddr == 0x8011c0);
-             nemu_assert(ph->p_align == 0x1000);
+             //nemu_assert(*(mybuf + 4096) == 0xbd);
+             //nemu_assert(ph->p_offset == 0x000000 || ph->p_offset == 0x1c0);
+             //nemu_assert(ph->p_vaddr == 0x800000 || ph->p_vaddr == 0x8011c0);
+             //nemu_assert(ph->p_align == 0x1000);
              //uint8_t mybuf[4096];
 			 //ramdisk_read(mybuf, *buf + ph->p_offset, ph->p_filesz);
              memcpy( (void *)ph->p_vaddr, &buf[ph->p_offset + ph->p_align], ph->p_filesz);
