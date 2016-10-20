cf8569aae9fb2457b3efaa054db9f3502555b07c
tracer-ics2016 <tracer@njuics.org>
2016-10-20 22:14:45 +0800

    >  compile kernel 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  22:14:45 up 6 days,  8:27, 19 users,  load average: 0.07, 0.13, 0.10 5e5e1b9b7bab3a20f3d50c3e611197b851708355

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -48,7 +48,7 @@ uint32_t loader() {
              //nemu_assert(0);
              uint8_t mybuf[0x9ffff];
              ramdisk_read(mybuf, 0, 0x9ffff);
-             nemu_assert(mybuf[0x1020] == 0x0c);
+             nemu_assert(mybuf[0x6c] == 0x07);
              //nemu_assert(ph->p_offset == 0x000000 || ph->p_offset == 0x1c0);
              //nemu_assert(ph->p_vaddr == 0x800000 || ph->p_vaddr == 0x8011c0);
              //nemu_assert(ph->p_align == 0x1000);
