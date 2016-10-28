3d05dba7c4015c794ca7a8199b0fc2c6b247b0ef
tracer-ics2016 <tracer@njuics.org>
2016-10-28 15:23:03 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  15:23:03 up 7 days,  4:07, 12 users,  load average: 0.00, 0.06, 0.06 453c86b8933d430cf9cbf975dee90766340fe0da

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -161,8 +161,11 @@ static int cmd_bt(char *args){
                 printf("%s (0x%x, 0x%x, 0x%x, 0x%x)\n", mystrtab + mysymtab[i].st_name, t1, t2, t3, t4);
             } 
         }
-        temp_eip = swaddr_read(ptr + 4, 4);
-        ptr = swaddr_read(ptr, 4);
+        if(ptr + 4 < 0x80000000)
+        {
+            temp_eip = swaddr_read(ptr + 4, 4);
+            ptr = swaddr_read(ptr, 4);
+        }
     }
     return 0;
 }
