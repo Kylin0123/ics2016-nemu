7805fec5d8ee502b3af0631815539b251a6b214d
tracer-ics2016 <tracer@njuics.org>
2016-10-29 13:27:10 +0000

    >  compile NEMU 151220015 czq Linux 1a599712f545 4.4.27-boot2docker #1 SMP Tue Oct 25 19:51:49 UTC 2016 x86_64 GNU/Linux  13:27:10 up 12:09,  1 user,  load average: 0.09, 0.08, 0.08 c5f1cca9f950bc6044c4755cb25ca60cd46711cb

--- lib-common/FLOAT/FLOAT_vfprintf.c
+++ lib-common/FLOAT/FLOAT_vfprintf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "FLOAT.h"
-//#include <sys/mman.h>
+#include <sys/mman.h>
 
 extern char _vfprintf_internal;
 extern char _fpmaxtostr;
@@ -39,7 +39,7 @@ static void modify_vfprintf() {
     
     int delta = (int)(format_FLOAT) - (int)&_fpmaxtostr;
     uint32_t addr = (uint32_t)&_vfprintf_internal + 0x306;
-    //mprotect((void *)((addr - 100) & 0xfffff000), 4096*2, PROT_READ | PROT_WRITE | PROT_EXEC);
+    mprotect((void *)((addr - 100) & 0xfffff000), 4096*2, PROT_READ | PROT_WRITE | PROT_EXEC);
     int *temp = (int *)(addr + 1);
     *temp += delta;
     *((char *)(addr - 0xa)) = 0xff;
@@ -95,7 +95,7 @@ extern char _ppfs_setargs;
 
 static void modify_ppfs_setargs() {
     uint32_t addr2change = (uint32_t)&_ppfs_setargs + 0x6f;
-    //mprotect((void *)((addr2change - 100) & 0xfffff000), 4096*2, PROT_READ | PROT_WRITE | PROT_EXEC);
+    mprotect((void *)((addr2change - 100) & 0xfffff000), 4096*2, PROT_READ | PROT_WRITE | PROT_EXEC);
     *((char *)(addr2change)) = 0xeb;
     *((char *)(addr2change + 0x1)) = 0x32;
 	/* TODO: Implement this function to modify the action of preparing
