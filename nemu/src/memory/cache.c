6676816b5f09597ff92dc6ec51edd42a3968eff0
tracer-ics2016 <tracer@njuics.org>
2016-11-04 14:03:18 +0000

    >  compile NEMU 151220015 czq Linux 1a599712f545 4.4.27-boot2docker #1 SMP Tue Oct 25 19:51:49 UTC 2016 x86_64 GNU/Linux  14:03:18 up  2:55,  1 user,  load average: 0.20, 0.09, 0.03 cb58bcd29ea688f9d0c7765ac6c6577b911c0f31

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -74,6 +74,7 @@ uint32_t read_cache(struct Cache* this, hwaddr_t addr, uint32_t *success, size_t
             }
         }
     }
+    printf("over\n");
     return unalign_rw(temp + temp_addr, 4);
 }
 
