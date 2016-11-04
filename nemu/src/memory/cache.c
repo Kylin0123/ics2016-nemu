94b2e2bec25bdf3de14040c17f3cfff8b512c7a5
tracer-ics2016 <tracer@njuics.org>
2016-11-04 12:41:25 +0000

    >  compile NEMU 151220015 czq Linux 1a599712f545 4.4.27-boot2docker #1 SMP Tue Oct 25 19:51:49 UTC 2016 x86_64 GNU/Linux  12:41:25 up  1:33,  1 user,  load average: 0.00, 0.00, 0.00 be22e5f493271d12662335a459b668fa1221a63b

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -37,14 +37,13 @@ uint32_t read_cache(struct Cache* this, hwaddr_t addr, uint32_t *success, size_t
     uint32_t temp_group = addr >> 6;
     temp_group &= 0x7f;
     uint32_t temp_addr = addr & 0x3f;
-    printf("cccc\n");
     *success = 0;
-    printf("aaaa\n");
     uint8_t temp[16];
     int i;
     for(i = 0; i < 8; i++){
         if(this->cache_block[temp_group][i].tag == temp_tag){
             if(this->cache_block[temp_group][i].valid_bit == 1){
+    printf("aaaa\n");
                 memcpy(temp, &this->cache_block[temp_group][i].data[temp_addr], len);
                 *success = 1;
                 if(temp_addr + len > 8){
