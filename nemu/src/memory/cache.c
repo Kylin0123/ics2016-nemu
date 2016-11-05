e4a88ce4ca985052b90135ae02f27e28d7e06948
tracer-ics2016 <tracer@njuics.org>
2016-11-05 14:56:38 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  14:56:38 up 7 days, 15:29, 14 users,  load average: 0.45, 0.23, 0.21 62e8f40f2d1bf370932472471cf17aff978e6d36

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -47,8 +47,8 @@ uint32_t read_cache(struct Cache* this, hwaddr_t addr, uint32_t *success, size_t
                 if(this->cache_block[temp_group][i].valid_bit == 1){
                     *success = 1;
                     //printf("cache_tag:%x ", this->cache_block[temp_group][i].tag);
-                    printf("addr:%x ", addr);
-                    printf("tag:%x group:%x addr:%x\n",temp_tag,temp_group, temp_addr);
+                    //printf("addr:%x ", addr);
+                    //printf("tag:%x group:%x addr:%x\n",temp_tag,temp_group, temp_addr);
                     memcpy(temp, this->cache_block[temp_group][i].data, 64);
                     break;
                 }
