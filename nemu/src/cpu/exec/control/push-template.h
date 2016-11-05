2285d261b2c6e272ee1185752f0e4f2179bd0891
tracer-ics2016 <tracer@njuics.org>
2016-11-05 12:16:36 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  12:16:36 up 7 days, 15:10, 14 users,  load average: 0.03, 0.07, 0.11 f75d512c278097ec80afc2e525e4c91114aad216

--- nemu/src/cpu/exec/control/push-template.h
+++ nemu/src/cpu/exec/control/push-template.h
@@ -19,67 +19,11 @@ static void do_execute(){
         cpu.esp -= 4;
         swaddr_write(cpu.esp, 4, op_src->val);
     }
-    //printf("pre_esp:%x\n", cpu.esp);
-    //DATA_TYPE temp = op_src->val;
-    //MEM_W(cpu.esp, temp);
-    //printf("DATA_BYTE:%d\n", DATA_BYTE);
     print_asm_template1();
-    /*if(cpu.eip == 0x100178){
-        printf("op_src:%d\n", temp);
-        uint32_t test = MEM_R(cpu.esp);
-        printf("esp_val:%x\n", test);
-    }*/
-    //printf("esp:%x\n\n", cpu.esp);
 }
 
 make_instr_helper(i)
 make_instr_helper(r)
 make_instr_helper(rm)
-//make_instr_helper(i2rm)
-/*
-make_helper(concat(push_ebp_, SUFFIX)){
-    op_src->val = cpu.ebp;
-    do_execute();
-    return 1;
-}
 
-make_helper(concat(push_edx_, SUFFIX)){
-    op_src->val = cpu.edx;
-    do_execute();
-    return 1;
-}
-
-make_helper(concat(push_eax_, SUFFIX)){
-    op_src->val = cpu.eax;
-    do_execute();
-    return 1;
-}
-*/
-/*
-#if DATA_BYTE == 2 || DATA_BYTE == 4
-make_helper(concat(push_m_, SUFFIX)){
-    concat(decode_rm_, SUFFIX)(eip + 1);
-    //printf("pre_op_src:%x\n", op_src->val);
-    int temp = instr_fetch(eip + 2, DATA_BYTE);
-    if(DATA_BYTE == 2){
-        if(MSB(temp) == 0)
-            temp = temp & 0x00ff;
-        else
-            temp = temp | 0xff00;
-    }
-    else if(DATA_BYTE == 4){
-        if(MSB(temp) == 0)
-            temp = temp & 0x000000ff;
-        else
-            temp = temp | 0xffffff00;
-    }
-    op_src->val += temp;
-    do_execute();
-    //printf("testValue:%d\n", DATA_BYTE);
-    //printf("op_src:%x\n", op_src->val);
-    //printf("temp:%d\n", temp);
-    return 3;
-}
-#endif
-*/
 #include "cpu/exec/template-end.h"
