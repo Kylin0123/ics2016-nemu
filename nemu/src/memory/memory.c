db0f63393b8a18e10394b95cff1425492463542c
tracer-ics2016 <tracer@njuics.org>
2016-11-05 16:09:14 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  16:09:14 up 7 days, 16:41, 14 users,  load average: 0.08, 0.14, 0.11 26d148e1b89bac644be0b7f541c892214bbf415c

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -20,19 +20,19 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
     if(*success == 1){
         //printf("hit!");
         //printf(" temp:%x\n", temp);
-        //free(success);
+        free(success);
         return temp & (~0u >> ((4 - len) << 3));
     }
     else{
         //printf("miss!");
-        //free(success);
+        free(success);
 	    return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
     }
     
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
-	//dram_write(addr, len, data);
+	dram_write(addr, len, data);
     
 }
 
