8e3a4a93592123e36679679e575a8a43270a2bd3
tracer-ics2016 <tracer@njuics.org>
2016-11-06 18:42:44 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  18:42:44 up 8 days,  2:24, 15 users,  load average: 0.17, 0.07, 0.06 61bd3a47593dc981e3b8886dd49d29349e67e7e

--- nemu/src/memory/cache2.c
+++ nemu/src/memory/cache2.c
@@ -130,52 +130,51 @@ void write_cache2(struct Cache2* this, hwaddr_t addr, uint32_t data, uint32_t *s
         if(this->cache_block2[temp_group][k].tag == temp_tag && this->cache_block2[temp_group][k].valid_bit == 1){
             *success2 = 1;
             //if(temp_addr + len > 64)
-                printf("tell me!\n");
+                //printf("tell me!\n");
             memcpy(this->cache_block2[temp_group][k].data + temp_addr, &data, 4);
             this->cache_block2[temp_group][k].dirty_bit = 1;
             //printf("write_cache2 hit!\n");   //hit!
             return;
         }
     }
-    int i = 0;
-    int flag = 0;
-    int result_i;
-    for(i = 0; i < 16; i++){
-        if(this->cache_block2[temp_group][i].valid_bit == 0){
-            result_i = i;
-            flag = 1;
-        }
-    }
-    if(flag == 0){
-        result_i = rand()%16;
-    }
-    //printf("result_i:%d\n", result_i);
-    if(this->cache_block2[temp_group][result_i].valid_bit == 1 && this->cache_block2[temp_group][result_i].dirty_bit == 1){
-        printf("dirty\n");
-        uint32_t dram_addr = (uint32_t)((this->cache_block2[temp_group][result_i].tag << 18) | (temp_group << 6) | temp_addr);
+    if(*success2 == 0){
+        int i = 0;
+        int flag = 0;
+        int result_i;
         for(i = 0; i < 16; i++){
-            uint32_t *dram_data = (uint32_t*)this->cache_block2[temp_group][result_i].data + i;
-            dram_write(dram_addr, 4, *dram_data);
-            
+            if(this->cache_block2[temp_group][i].valid_bit == 0){
+                result_i = i;
+                flag = 1;
+            }
         }
+        if(flag == 0){
+            result_i = rand()%16;
+        }
+        //printf("result_i:%d\n", result_i);
+        if(this->cache_block2[temp_group][result_i].valid_bit == 1 && this->cache_block2[temp_group][result_i].dirty_bit == 1){
+            printf("dirty\n");
+            uint32_t dram_addr = (uint32_t)((this->cache_block2[temp_group][result_i].tag << 18) | (temp_group << 6) | temp_addr);
+            for(i = 0; i < 16; i++){
+                uint32_t *dram_data = (uint32_t*)this->cache_block2[temp_group][result_i].data + i;
+                dram_write(dram_addr, 4, *dram_data);
+            
+            }
         
+        }
+        this->cache_block2[temp_group][result_i].valid_bit = 1;
+        this->cache_block2[temp_group][result_i].tag = temp_tag;
+        uint8_t temp2[64];
+        uint32_t align_addr = addr & 0xffffffc0;
+        //printf("addr:0x%x\n", align_addr);
+        //printf("dram_read:0x%x\n", dram_read(align_addr,4));
+        int j;
+        for(j = 0; j < 64; j++){
+            temp2[j] = dram_read(align_addr + j, 1);
+            memcpy(this->cache_block2[temp_group][result_i].data + j, temp2 + j, 1);
+            //printf("%x ", temp2[j]);
+        }
+        //printf("\n");
+        memcpy(this->cache_block2[temp_group][result_i].data + temp_addr, &data, 4);
+        this->cache_block2[temp_group][result_i].dirty_bit = 1;
     }
-    this->cache_block2[temp_group][result_i].valid_bit = 1;
-    this->cache_block2[temp_group][result_i].tag = temp_tag;
-    uint8_t temp2[64];
-    uint32_t align_addr = addr & 0xffffffc0;
-    //printf("addr:0x%x\n", align_addr);
-    //printf("dram_read:0x%x\n", dram_read(align_addr,4));
-    int j;
-    for(j = 0; j < 64; j++){
-        temp2[j] = dram_read(align_addr + j, 1);
-        memcpy(this->cache_block2[temp_group][result_i].data + j, temp2 + j, 1);
-        //printf("%x ", temp2[j]);
-    }
-    //printf("\n");
-    memcpy(this->cache_block2[temp_group][result_i].data + temp_addr, &data, 4);
-    this->cache_block2[temp_group][result_i].dirty_bit = 1;
-    
-
-
 }
