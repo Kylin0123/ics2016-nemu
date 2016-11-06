d05c2f576ea321821c9f516af774c48b4d3b8889
tracer-ics2016 <tracer@njuics.org>
2016-11-06 13:59:49 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  13:59:49 up 7 days, 22:46, 14 users,  load average: 0.21, 0.22, 0.17 daeb6550dc26849bb2bab9bdfa27dd531745fd6b

--- nemu/src/memory/cache2.c
+++ nemu/src/memory/cache2.c
@@ -87,6 +87,7 @@ uint32_t read_cache2(struct Cache2* this, hwaddr_t addr, uint32_t *success2, siz
             temp2[j] = dram_read(align_addr + j, 1);
             memcpy(this->cache_block2[temp_group][result_i].data + j, temp2 + j, 1);
         }
+        printf("read_cache2 miss!\n");
         return unalign_rw(temp2 + temp_addr, 4);
     }
     printf("read_cache2 hit!\n"); //hit!
