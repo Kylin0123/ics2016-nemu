ac9b59d79b16ba69410d029a964436157dc11407
tracer-ics2016 <tracer@njuics.org>
2016-11-26 10:51:27 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  10:51:27 up 9 days, 11:21, 34 users,  load average: 0.14, 0.21, 0.14 e8bd3bfdbd6102a6f4bc0c4fdc8280a6433f396

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -28,6 +28,7 @@ void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
 	//return hwaddr_read(addr, len);
     if(cpu.cr0.protect_enable == 0 || cpu.cr0.paging == 0)
+        return addr;
 	assert(len == 1 || len == 2 || len == 4);
     if((addr & 0xfff) + len > 0x1000)
         assert(0);
