2917b6b08dbf03a3178cc1dfc69efd35826d097b
tracer-ics2016 <tracer@njuics.org>
2016-11-29 20:16:26 +0800

    >  compile kernel 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  20:16:26 up 9 days, 16:59, 35 users,  load average: 0.00, 0.06, 0.11 d84d449e8d94c8443130ddc7f38c009e766283db

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -45,8 +45,8 @@ uint32_t loader() {
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-             nemu_assert(0);
              uint32_t hwaddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
+             nemu_assert(0);
              ramdisk_read((uint8_t *)hwaddr, ph->p_offset, ph->p_filesz);
              //memcpy( (void *)(ph->p_vaddr), (void *)ph->p_offset, ph->p_filesz);
 			/* TODO: zero the memory region 
