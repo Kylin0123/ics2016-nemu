2a9c83c8f0d13ca7020a3d1e505a6184a909048f
tracer-ics2016 <tracer@njuics.org>
2016-11-29 21:07:26 +0800

    >  compile kernel 151220015 user Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  21:07:26 up 9 days, 17:50, 35 users,  load average: 0.20, 0.22, 0.21 392de57a8113c629f4a64512f8af6dcfc5115681

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -34,30 +34,30 @@ uint32_t loader() {
 	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
-    //ph = (Elf32_Phdr *)(buf + elf->e_phoff); 
+    ph = (Elf32_Phdr *)(buf + elf->e_ehsize); 
 	/* Load each program segment */
 	//panic("please implement me");
     int i;
 	for(i = 0; i < elf->e_phnum; i++) {
 		/* Scan the program header table, load each segment into memory */
         
-        ph = (void *)buf + elf->e_phoff + i*elf->e_phentsize;
+        //ph = (void *)buf + elf->e_phoff + i*elf->e_phentsize;
 		
         if(ph->p_type == PT_LOAD) {
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
              
-            uint32_t hwaddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
+            uint32_t hwaddr = mm_malloc(ph[i].p_vaddr, ph[i].p_memsz);
              
-            ramdisk_read((uint8_t *)hwaddr, ph->p_offset, ph->p_filesz);
+            ramdisk_read((uint8_t *)hwaddr, ph[i].p_offset, ph[i].p_filesz);
             
             //memcpy( (void *)(ph->p_vaddr), (void *)ph->p_offset, ph->p_filesz);
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
 
-             memset((void *)hwaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
+             memset((void *)hwaddr + ph[i].p_filesz, 0, ph[i].p_memsz - ph[i].p_filesz);
              
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
