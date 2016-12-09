f61e66c3891b14e4f8429a83de53fc78c1a06dc6
tracer-ics2016 <tracer@njuics.org>
2016-12-09 20:15:38 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-47-generic #68-Ubuntu SMP Wed Oct 26 19:39:59 UTC 2016 i686 i686 i686 GNU/Linux  20:15:38 up 16:15, 12 users,  load average: 0.03, 0.07, 0.07 49276760c8780bfbdae63451c114d87959755532

--- nemu/src/cpu/exec/control/push.c
+++ nemu/src/cpu/exec/control/push.c
@@ -22,4 +22,33 @@
 make_helper_v(push_i)
 make_helper_v(push_r)
 make_helper_v(push_rm)
-//make_helper_v(push_m)
+
+make_helper(pusha){
+    uint32_t temp = cpu.esp;
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, cpu.eax, R_SS);
+
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, cpu.ecx, R_SS);
+    
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, cpu.edx, R_SS);
+
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, cpu.ebx, R_SS);
+
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, temp, R_SS);
+
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, cpu.ebp, R_SS);
+
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, cpu.esi, R_SS);
+
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, cpu.edi, R_SS);
+
+    return 1;
+}
+
