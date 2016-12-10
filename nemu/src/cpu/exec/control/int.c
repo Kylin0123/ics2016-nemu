1fddf8fccd11483a7241c6c38310280379c996db
tracer-ics2016 <tracer@njuics.org>
2016-12-10 17:07:21 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-47-generic #68-Ubuntu SMP Wed Oct 26 19:39:59 UTC 2016 i686 i686 i686 GNU/Linux  17:07:21 up 1 day, 53 min, 18 users,  load average: 0.08, 0.02, 0.01 da5db37ded28a4c65d849bbf6816432770b96352

--- nemu/src/cpu/exec/control/int.c
+++ nemu/src/cpu/exec/control/int.c
@@ -7,6 +7,11 @@
 
 #include "cpu/exec/helper.h"
 
-#define DATA_BYTE 1
-#include "int-template.h"
-#undef DATA_BYTE
+extern void raise_intr(uint8_t NO);
+
+make_helper(Int_i_b){
+    int NO = instr_fetch(eip + 1, 1);
+    cpu.eip += 2;
+    raise_intr(NO);
+    return 0;
+}
