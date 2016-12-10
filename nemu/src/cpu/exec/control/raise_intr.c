a2a6d01d27b4850bde6de7e1485f480fabeac495
tracer-ics2016 <tracer@njuics.org>
2016-12-10 17:15:04 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-47-generic #68-Ubuntu SMP Wed Oct 26 19:39:59 UTC 2016 i686 i686 i686 GNU/Linux  17:15:04 up 1 day,  1:01, 18 users,  load average: 0.23, 0.07, 0.02 4e2080a22dea03d91f47442cb0f903c0346e91af

--- nemu/src/cpu/exec/control/raise_intr.c
+++ nemu/src/cpu/exec/control/raise_intr.c
@@ -49,7 +49,7 @@ void raise_intr(uint8_t NO) {
     cpu.esp -= 4;
     swaddr_write(cpu.esp, 4, R_SS, cpu.eip);
     printf("raise_intr.c eip:%x\n",cpu.eip);
-    assert(0);
+    //assert(0);
 
     print_asm("int 0x%x", NO);
     Assert(NO <= cpu.idtr.limit, "NO %d is out if range\n", NO);
