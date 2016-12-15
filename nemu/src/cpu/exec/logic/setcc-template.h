49b168cdb4c89bc432f7af36e6860e63b57802da
tracer-ics2016 <tracer@njuics.org>
2016-12-15 10:14:33 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-47-generic #68-Ubuntu SMP Wed Oct 26 19:39:59 UTC 2016 i686 i686 i686 GNU/Linux  10:14:32 up 1 day, 14:32, 24 users,  load average: 0.19, 0.11, 0.04 fb02062ee4b1b0f6e832c4f31ddd44f8ed074fc8

--- nemu/src/cpu/exec/logic/setcc-template.h
+++ nemu/src/cpu/exec/logic/setcc-template.h
@@ -35,6 +35,202 @@ make_instr_helper(rm)
 
 #undef instr
 
+#define instr seta
+
+static void do_execute(){
+    if(cpu.eflags._cf == 0 && cpu.eflags._zf == 0)
+        OPERAND_W(op_src, 1);
+    else
+        OPERAND_W(op_src, 0);
+    print_asm_template1();
+}
+
+make_instr_helper(rm)
+
+#undef instr
+
+#define instr setbe
+
+static void do_execute(){
+    if(cpu.eflags._cf == 1 || cpu.eflags._zf == 1)
+        OPERAND_W(op_src, 1);
+    else
+        OPERAND_W(op_src, 0);
+    print_asm_template1();
+}
+
+make_instr_helper(rm)
+
+#undef instr
+
+#define instr setae
+
+static void do_execute(){
+    if(cpu.eflags._cf == 0)
+        OPERAND_W(op_src, 1);
+    else
+        OPERAND_W(op_src, 0);
+    print_asm_template1();
+}
+
+make_instr_helper(rm)
+
+#undef instr
+
+#define instr setb
+
+static void do_execute(){
+    if(cpu.eflags._cf == 1)
+        OPERAND_W(op_src, 1);
+    else
+        OPERAND_W(op_src, 0);
+    print_asm_template1();
+}
+
+make_instr_helper(rm)
+
+#undef instr
+
+#define instr seto
+
+static void do_execute(){
+    if(cpu.eflags._of == 1)
+        OPERAND_W(op_src, 1);
+    else
+        OPERAND_W(op_src, 0);
+    print_asm_template1();
+}
+
+make_instr_helper(rm)
+
+#undef instr
+
+#define instr setno
+
+static void do_execute(){
+    if(cpu.eflags._of == 0)
+        OPERAND_W(op_src, 1);
+    else
+        OPERAND_W(op_src, 0);
+    print_asm_template1();
+}
+
+make_instr_helper(rm)
+
+#undef instr
+
+#define instr setg
+
+static void do_execute(){
+    if(cpu.eflags._zf == 0 && cpu.eflags._sf == cpu.eflags._of)
+        OPERAND_W(op_src, 1);
+    else
+        OPERAND_W(op_src, 0);
+    print_asm_template1();
+}
+
+make_instr_helper(rm)
+
+#undef instr
+
+#define instr setge
+
+static void do_execute(){
+    if(cpu.eflags._sf == cpu.eflags._of)
+        OPERAND_W(op_src, 1);
+    else
+        OPERAND_W(op_src, 0);
+    print_asm_template1();
+}
+
+make_instr_helper(rm)
+
+#undef instr
+
+#define instr setl
+
+static void do_execute(){
+    if(cpu.eflags._sf != cpu.eflags._of)
+        OPERAND_W(op_src, 1);
+    else
+        OPERAND_W(op_src, 0);
+    print_asm_template1();
+}
+
+make_instr_helper(rm)
+
+#undef instr
+
+#define instr setle
+
+static void do_execute(){
+    if(cpu.eflags._zf == 1 || cpu.eflags._sf != cpu.eflags._of)
+        OPERAND_W(op_src, 1);
+    else
+        OPERAND_W(op_src, 0);
+    print_asm_template1();
+}
+
+make_instr_helper(rm)
+
+#undef instr
+
+#define instr setnp
+
+static void do_execute(){
+    if(cpu.eflags._pf == 0)
+        OPERAND_W(op_src, 1);
+    else
+        OPERAND_W(op_src, 0);
+    print_asm_template1();
+}
+
+make_instr_helper(rm)
+
+#undef instr
+
+#define instr setns
+
+static void do_execute(){
+    if(cpu.eflags._sf == 0)
+        OPERAND_W(op_src, 1);
+    else
+        OPERAND_W(op_src, 0);
+    print_asm_template1();
+}
+
+make_instr_helper(rm)
+
+#undef instr
+
+#define instr setp
+
+static void do_execute(){
+    if(cpu.eflags._pf == 1)
+        OPERAND_W(op_src, 1);
+    else
+        OPERAND_W(op_src, 0);
+    print_asm_template1();
+}
+
+make_instr_helper(rm)
+
+#undef instr
+
+#define instr sets
+
+static void do_execute(){
+    if(cpu.eflags._sf == 1)
+        OPERAND_W(op_src, 1);
+    else
+        OPERAND_W(op_src, 0);
+    print_asm_template1();
+}
+
+make_instr_helper(rm)
+
+#undef instr
+
 #include "cpu/exec/template-end.h"
 
 
