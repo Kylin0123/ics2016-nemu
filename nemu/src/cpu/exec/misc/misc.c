db3cdf1f34fdf4d53462e6d5a408c425071eb007
tracer-ics2016 <tracer@njuics.org>
2016-12-21 16:16:35 +0800

    >  compile NEMU 151220015 user Linux ubuntu 4.4.0-53-generic #74-Ubuntu SMP Fri Dec 2 15:58:04 UTC 2016 i686 i686 i686 GNU/Linux  16:16:35 up 19:06, 16 users,  load average: 5.54, 5.49, 5.31 ca62e64b5e52d253784e4229103c6501e3ac7b37

--- nemu/src/cpu/exec/misc/misc.c
+++ nemu/src/cpu/exec/misc/misc.c
@@ -74,9 +74,8 @@ make_helper(sti) {
 make_helper(hlt) {
     assert(cpu.eflags._if == 1);
     while(1){
-        //Log("intr:%d eflags:%d",cpu.INTR,cpu.eflags._if);
-        volatile uint8_t temp = cpu.INTR & cpu.eflags._if;
-        if(temp){
+        Log("intr:%d eflags:%d",cpu.INTR,cpu.eflags._if);
+        if(cpu.INTR & cpu.eflags._if){
             break;
         }
 
